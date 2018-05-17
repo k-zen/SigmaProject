0f769750280bd2a098279bd5e88366b36c1416c2
K-Zen <akc@apkc.net>
2018-05-17 10:16:28 -0700

    Miscellaneous.

--- setup.py
+++ setup.py
@@ -12,19 +12,9 @@ setup(
     long_description=open('README.txt').read(),
     entry_points={'console_scripts': ['sigmaproject = sigmaproject.__main__:main']},
     install_requires=[
-        "wradlib >= 0.9",
-        "haversine >= 0.4.5",
-        "numpy >= 1.6.1",
-        "matplotlib	>= 1.1.0",
-        "scipy >= 0.9",
-        "h5py >= 2.0.1",
-        "netCDF4 >= 1.0",
-        "gdal >= 1.9",
-        "watchdog >= 0.8.3",
-        "requests >= 2.13.0",
-        "geopy",
-        "scikit-learn",
-        "shapely",
-        "pandas"
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "seaborn"
     ]
 )
